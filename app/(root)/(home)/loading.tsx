6fd879164059d81713b57905ac5bb85c7028f66b
your Friends from the Neighborhood <freundeausdernachbarschaft@gmail.com>
2023-09-30 01:51:42 +0200

    fix skeleton col or, improve metadata, change favicon

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -5,15 +5,15 @@ const loading = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <Skeleton className= "h-[274px] w-full rounded-lg bg-red-500" />
+        <Skeleton className= "h-[274px] w-full rounded-lg  bg-[green]" />
       </section>
 
       <section className="mt-6 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56" />
+        <Skeleton className="h-10 w-56 bg-[green]" />
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-        <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-        <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-        <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+        <Skeleton className="h-[440px] w-full sm:w-[356px] bg-[green]" />
+        <Skeleton className="h-[440px] w-full sm:w-[356px] bg-[green]" />
+        <Skeleton className="h-[440px] w-full sm:w-[356px] bg-[green]" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,19 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Papierfische',
   description: 'Papierfische Infomaterial',
+  other: {
+    'theme-color': '#082e0b',
+    //the color on mobile devices
+    "color-scheme": "dark only",
+    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    //the image/info ppl see if u share a link
+    "twitter:card": "summary_large_image",
+    "og:url": "papierfische",
+    //describe main url
+    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    //if u share the webapp eg in discord or messengers/apps this appears
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
